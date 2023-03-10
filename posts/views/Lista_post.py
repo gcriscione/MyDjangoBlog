05978e9e31baf1f3da1af3cca871d8d6aa011d17
Giovanni Criscione <giovanni2001rg@gmail.com>
2023-03-10 15:55:55 +0100

    Creato sistema di template (bootstrap)

--- posts/views/Lista_post.py
+++ posts/views/Lista_post.py
@@ -4,4 +4,5 @@ from posts.models import Post
 # Create your views here.
 class Lista_post(ListView):
     template_name = 'posts\lista_post.html'
-    queryset = Post.objects.all().order_by("-data")
+    queryset = Post.objects.all().order_by("data")
+    paginate_by = 5;
